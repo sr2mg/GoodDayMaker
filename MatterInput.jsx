29e081f9bae20efdd9fb9d6bea5f9f1e45e033ae
sr2mg <55787575+sr2mg@users.noreply.github.com>
2020-02-19 13:47:41 +0900

    Add files via upload

--- MatterInput.jsx
+++ MatterInput.jsx
@@ -1,42 +1,42 @@
 function MatterInput(props){
-    //参照の取得
-    //デフォルトがnull
-    //参照したい要素にjsxでrefをセットする
-    const textareaRef = React.useRef(null);
-    const numberRef = React.useRef(null);
+	//参照の取得
+	//デフォルトがnull
+	//参照したい要素にjsxでrefをセットする
+	const textareaRef = React.useRef(null);
+	const numberRef = React.useRef(null);
 
-    const checkNum=(hoge)=>{
-        let checkNum=isNaN(hoge);
-        if(hoge>20||hoge<-20)checkNum=true;
-        if(hoge=="")checkNum=true;
-        return !checkNum;
-    }
+	const checkNum=(hoge)=>{
+		let checkNum=isNaN(hoge);
+		if(hoge>20||hoge<-20)checkNum=true;
+		if(hoge=="")checkNum=true;
+		return !checkNum;
+	};
 
-    const sendMatter = React.useCallback(()=>{
-        //参照した先の現在値を渡す
-        //addMatterとaddValはmainのpropsでもらっている
-        if(checkNum(numberRef.current.value)){
-            props.addMatter({
-                id: new Date().getTime(),//idをユニークにしたい
-                //number: 2,//ここに現在の配列番号を入れたい
-                content:textareaRef.current.value,
-                goodvalue:numberRef.current.value,
+	const sendMatter = React.useCallback(()=>{
+		//参照した先の現在値を渡す
+		//addMatterとaddValはmainのpropsでもらっている
+		if(checkNum(numberRef.current.value)){
+			props.addMatter({
+				id: new Date().getTime(),//idをユニークにしたい
+				//number: 2,//ここに現在の配列番号を入れたい
+				content:textareaRef.current.value,
+				goodvalue:numberRef.current.value,
 
-            });
-            props.addVal(numberRef.current.value);
-        }
-    
-    },[textareaRef.current,props.addMatter,numberRef]);
-    
+			});
+			props.addVal(numberRef.current.value);
+		}
+	
+	},[textareaRef.current,props.addMatter,numberRef]);
+	
 
-    //ここでrefをセット
-    return(
-        <div>
-            <div>出来事</div>
-            <div><textarea className="matter-textarea" ref={textareaRef} required></textarea></div>
-            <div>良かった度</div>
-            <div><input type="number" className="number-area" ref={numberRef} min="-20" max="20" required></input></div>
-            <div onClick={sendMatter} className="send-matter">幸福決定</div>
-        </div>
-    );
+	//ここでrefをセット
+	return(
+		<div>
+			<div>出来事</div>
+			<div><textarea className="matter-textarea" ref={textareaRef} required></textarea></div>
+			<div>良かった度</div>
+			<div><input type="number" className="number-area" ref={numberRef} min="-20" max="20" required></input></div>
+			<div onClick={sendMatter} className="send-matter">幸福決定</div>
+		</div>
+	);
 }
--- main.jsx
+++ main.jsx
@@ -1,41 +1,41 @@
 function App(){
 
-    const [matters,setMatters]=React.useState([
+	const [matters,setMatters]=React.useState([
 
-        {
-            id:0,
-            content:'これを起動した',
-            goodvalue:5
-        }
-    ]);
+		{
+			id:0,
+			content:"これを起動した",
+			goodvalue:5
+		}
+	]);
 
-    const [GoodVal,setVal] =React.useState(10)//デフォが0
+	const [GoodVal,setVal] =React.useState(10)//デフォが0
 
-    //usecallbackを用いたaddMatter関数の定義。setmattersにて今までのmatterにプラスでやれる
-    const addMatter = React.useCallback((matter)=>setMatters((prev)=>[...prev,matter]),[setMatters]);
-    const eraseMatter = React.useCallback((val)=>setMatters((prev)=>[...prev.slice(0,val),...prev.slice(val+1)]),[setMatters]);
+	//usecallbackを用いたaddMatter関数の定義。setmattersにて今までのmatterにプラスでやれる
+	const addMatter = React.useCallback((matter)=>setMatters((prev)=>[...prev,matter]),[setMatters]);
+	const eraseMatter = React.useCallback((val)=>setMatters((prev)=>[...prev.slice(0,val),...prev.slice(val+1)]),[setMatters]);
 
-    //計算の関数の定義。これをaddvalとしてMatterInputに渡している。
-    const addVal = React.useCallback((GoodVal)=>setVal((prev)=>parseInt(prev)+parseInt(GoodVal)),[setVal]);
+	//計算の関数の定義。これをaddvalとしてMatterInputに渡している。
+	const addVal = React.useCallback((GoodVal)=>setVal((prev)=>parseInt(prev)+parseInt(GoodVal)),[setVal]);
 
-    //削除したとき、選択したものがあるかいなか
+	//削除したとき、選択したものがあるかいなか
 
 
-    const erase =(val)=>{
-        eraseMatter(val);
-    }
+	const erase =(val)=>{
+		eraseMatter(val);
+	};
 
-    //return部分多分完成
-    return(
-        <div>
-            <MatterInput addMatter ={addMatter} addVal={addVal} matters={matters} />
-            <div>今日の幸福度:{GoodVal}</div>
-            <GBlist matters ={matters} eraseMatter={erase}/>
-            
-        </div>
-    );
+	//return部分多分完成
+	return(
+		<div>
+			<MatterInput addMatter ={addMatter} addVal={addVal} matters={matters} />
+			<div>今日の幸福度:{GoodVal}</div>
+			<GBlist matters ={matters} eraseMatter={erase}/>
+			
+		</div>
+	);
 }
 
 //render部分
-const target =document.querySelector('#app');
+const target =document.querySelector("#app");
 ReactDOM.render(<App/>,target);
