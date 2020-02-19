0cda381dd17c691815646c1c7f62999ed09734fc
sr2mg <55787575+sr2mg@users.noreply.github.com>
2020-02-19 14:20:30 +0900

    eslint導入

--- GBlist.jsx
+++ GBlist.jsx
@@ -1,20 +1,20 @@
 function GBlist(props){
 
-    //main→GBlist→matter
-    const MatterList = props.matters.map((mt)=>(
-        <Matter
-            key={mt.id}
-            id={mt.id}
-            goodvalue={mt.goodvalue}
-            content={mt.content}
-            eraseMatter={props.eraseMatter}
-        />
+	//main→GBlist→matter
+	const MatterList = props.matters.map((mt)=>(
+		<Matter
+			key={mt.id}
+			id={mt.id}
+			goodvalue={mt.goodvalue}
+			content={mt.content}
+			eraseMatter={props.eraseMatter}
+		/>
 
-    ));
+	));
 
-    return( 
-    <div>
-        <div className="GBlist">{MatterList}</div>
-    </div>
-    );
+	return( 
+	<div>
+		<div className="GBlist">{MatterList}</div>
+	</div>
+	);
 }
--- Matter.jsx
+++ Matter.jsx
@@ -1,35 +1,35 @@
 function Matter(props){
 //if props.goodvalue<0 className="badmatter"
-    let isgoo=true;
-    
-    const [visibled,setVisibled]=React.useState(true);
-    //Matterのvisible変更用
-    const eraseMatter = React.useCallback((visibled)=>setVisibled((prev)=>!prev),[setVisibled]);
+	let isgoo=true;
+	
+	const [visibled,setVisibled]=React.useState(true);
+	//Matterのvisible変更用
+	const eraseMatter = React.useCallback((visibled)=>setVisibled((prev)=>!prev),[setVisibled]);
 
-    const erase=React.useCallback(()=>{
-        eraseMatter();
-        props.subVal(props.goodvalue);
-    });
+	const erase=React.useCallback(()=>{
+		eraseMatter();
+		props.subVal(props.goodvalue);
+	});
 
-    const VisCheck=()=>{
-        if(visibled){
-            return(
-                <div className={[isgoo ? 'good' :'bad','matter'].join('')}>
-                <div className="content">出来事：{props.content}</div>
-                <div className="goodvalue">良かった度：{props.goodvalue}</div>
-                <div className="erase-Button" onClick={erase}>削除</div>
-            </div>
-            )
-        }else{
-            return (
-            <div></div>
-            )
-        }
-    }
-    
+	const VisCheck=()=>{
+		if(visibled){
+			return(
+			    <div className={[isgoo ? 'good' :'bad','matter'].join('')}>
+				    <div className="content">出来事：{props.content}</div>
+				    <div className="goodvalue">良かった度：{props.goodvalue}</div>
+				    <div className="erase-Button" onClick={erase}>削除</div>
+			    </div>
+			);
+		}else{
+			return (
+                <div></div>
+			);
+		}
+	}
+	
 
-    if(props.goodvalue<0)isgoo=false;
-    return(
-        <VisCheck/>
-    );
+	if(props.goodvalue<0)isgoo=false;
+	return(
+		<VisCheck/>
+	);
 }
